f81c14b30cdc1a8a10df1db3bcd7e0a0f2286748
jameskiprop <jameskiprop01@gmail.com>
2024-08-04 19:52:00 +0300

    progress on the es6 coursework

--- file.js
+++ file.js
@@ -1,6 +1,6 @@
 //Arrow function js
 
-/*class Employee {
+class Employee {
   constructor(name) {
     this.name = name;
   }
@@ -20,9 +20,9 @@
 
 let firstEmployee = new Employee("John Doe");
 firstEmployee.getEmployeeName();
-firstEmployee.getEmployeeNameArrow();*/
+firstEmployee.getEmployeeNameArrow();
 
-// argument
+argument;
 
 let sayHello = () => alert`hello `;
 let saybye = () => alert`Bye`;
@@ -36,3 +36,97 @@ let doSomething = (somethingToDo) => {
 //any function that will be sent to dosomething() will be  called right away
 doSomething(sayHello);
 doSomething(saybye);
+
+//Array;
+const people = [{ name: "Max" }, { name: "Jack" }, { name: "Mary" }];
+
+let i = 0;
+
+while (i < people.length && people[i].name != "Jack") {
+  i++;
+}
+
+if (i !== people.length) {
+  alert("Jack is in" + i);
+} else {
+  ("cannot find Jack");
+}
+
+//Array function find in Javascript
+
+const people = [{ name: "Max" }, { name: "Max" }, { name: "Max" }];
+
+function findPerson(name) {
+  for (let i = 0; i < people.length; i++) {
+    let person = people[i];
+    if (person.name == name) {
+      return person;
+    }
+  }
+}
+
+//Es6
+function findPerson(name) {
+  return people.find((person) => person.name == name);
+}
+
+//Array function forEach
+const civillians = [{ name: "Lucy" }, { name: "Jane" }, { name: "James" }];
+
+//js
+
+function showEachPerson(name) {
+  for (let i = 0; i < people.length; i++) {
+    alert(people[i].name);
+  }
+}
+//ES6
+const showEachPerson = (name) => people.forEach((person) => alert(person.name));
+
+//filter method
+const products = [
+  { name: "milk", price: "14" },
+  { name: "Bread", price: "55" },
+  { name: "cheese", price: "25" },
+];
+
+//javascript
+function filterProducts() {
+  let cheapProducts = []; //where cheap products will be stored
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].price < 10) cheapProducts.push(products[i]);
+  }
+  return cheapProducts;
+}
+
+//es6
+const filterProducts = () => products.filter((product) => product.price < 10);
+
+const numbers = [1, 5, 6, -5, -7, 9, 3, 2];
+
+const filterNumbers = () => numbers.filter((number) => number > 0);
+
+console.log(filterNumbers());
+
+//USING MAP METHOD (executes the same instruction on every element in a given array, done on a copy of the array)
+const products = [
+  { name: "milk", price: "15" },
+  { name: "water", price: "9" },
+  { name: "shoe", price: "26" },
+];
+
+//js
+function changeProducts() {
+  for (let i = 0; i < products.length; i++) {
+    products[i].price += 2;
+  }
+  return products;
+}
+
+//es6
+const changeProducts = () =>
+  products.map((product) => ({ ...product, price: product.price + 2 }));
+
+console.log(changeProducts());
+
+console.log(products);
